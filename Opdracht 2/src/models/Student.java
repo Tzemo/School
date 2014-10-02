88f60eebdd088ccbd8e3a52b0e3fd4e25cf1f525
Duy Tran <zeromus45@gmail.com>
2014-10-02 10:24:35 +0200

    Project
    project

---
+++ Opdracht 2/src/models/Student.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package models;
+
+/**
+ *
+ * @author duytran
+ */
+public class Student {
+    
+    private int id;
+    private String name;
+    
+    
+}
